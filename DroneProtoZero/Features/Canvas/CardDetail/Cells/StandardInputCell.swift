92c38693f7a52a3987285c9cd177b899b8bb6976
Saad Ismail <saad@us.ibm.com>
2017-03-21 11:24:37 -0400

    fixing cyclomatic complexity issues.. theres no other way to make this simpler.. we need guards inside the switch statements and it wouldn’t make sense to split each case into a different function.

--- DroneProtoZero/Features/Canvas/CardDetail/Cells/StandardInputCell.swift
+++ DroneProtoZero/Features/Canvas/CardDetail/Cells/StandardInputCell.swift
@@ -42,6 +42,7 @@ class StandardInputCell: CardDetailTableViewCell, CardDetailInputCell {
         input?.text = getSelectedInputOption()
     }
     
+    //swiftlint:disable:next cyclomatic_complexity
     func getSelectedInputOption() -> String {
         guard let card = self.actionCard,
             let inputSlot = self.inputSlot else {
